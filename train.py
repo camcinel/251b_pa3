fixed mean iou calculation, added second implementation of focal loss (FocalLoss2), removed a bunch of TODOs

train.py
@@ -24,7 +24,7 @@ def init_weights(m):
         torch.nn.init.normal_(m.bias.data)  # xavier not applicable for biases
 
 
-# TODO Get class weights
+# Get class weights
 def getClassWeights(dataset, n_classes=21, device='cpu'):
     n_sample = torch.zeros(n_classes).to(device=device)
     total_samples = torch.zeros(1).to(device=device)
@@ -57,24 +57,24 @@ n_class = 21
 patience = 25
 
 if torch.cuda.is_available():
-    device = 'cuda'  # TODO determine which device to use (cuda or cpu)
+    device = 'cuda'
 else:
     device = 'cpu'
 
 fcn_model = UNet(n_class=n_class).to(device=device)
 fcn_model.apply(init_weights)
 
-optimizer = torch.optim.Adam(fcn_model.parameters(), lr=learning_rate)  # TODO choose an optimizer
+optimizer = torch.optim.Adam(fcn_model.parameters(), lr=learning_rate)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs)
 criterion = torch.nn.CrossEntropyLoss(weight=getClassWeights(train_dataset, n_classes=n_class, device=device),
-                                      reduction='mean')  # TODO Choose an appropriate loss function from https://pytorch.org/docs/stable/_modules/torch/nn/modules/loss.html
+                                      reduction='mean')
 
 
 # criterion = torch.nn.CrossEntropyLoss()
 
 
 def train(give_time=False):
-    best_iou_score = 0.0
+    best_loss = np.inf
     counter = 0
     train_loss = []
     val_loss = []
@@ -86,30 +86,32 @@ def train(give_time=False):
     for epoch in range(epochs):
         ts = time.time()
         epoch_loss = []
-        epoch_iou = []
         epoch_acc = []
+        intersection = np.zeros(n_class)
+        union = np.zeros(n_class)
         for iter, (inputs, labels) in enumerate(train_loader):
-            # TODO  reset optimizer gradients
+            # reset optimizer gradients
             optimizer.zero_grad()
 
             # both inputs and labels have to reside in the same device as the model's
-            inputs = inputs.to(device=device)  # TODO transfer the input to the same device as the model's
-            labels = labels.to(device=device)  # TODO transfer the labels to the same device as the model's
+            inputs = inputs.to(device=device)  # transfer the input to the same device as the model's
+            labels = labels.to(device=device)  # transfer the labels to the same device as the model's
 
             outputs = fcn_model(
-                inputs)  # TODO  Compute outputs. we will not need to transfer the output, it will be automatically in the same device as the model's!
+                inputs)  # Compute outputs
 
-            loss = criterion(outputs, labels)  # TODO  calculate loss
-            iou = util.iou(outputs, labels.clone().detach())
+            loss = criterion(outputs, labels)  # calculate loss
+            batch_intersection, batch_union = util.iou(outputs, labels.clone().detach())
+            intersection += batch_intersection
+            union += batch_union
             acc = util.pixel_acc(outputs, labels.clone().detach())
             epoch_loss.append(loss.item())
-            epoch_iou.append(iou)
             epoch_acc.append(acc)
 
-            # TODO  backpropagate
+            # backpropagate
             loss.backward()
 
-            # TODO  update the weights
+            # update the weights
             optimizer.step()
 
             if iter % 10 == 0:
@@ -120,21 +122,24 @@ def train(give_time=False):
             print("Finish epoch %d\tTime elapsed %.4f seconds" % (epoch, time.time() - ts))
 
         current_loss, current_miou_score, current_acc = val(epoch)
+
+        train_mean_iou = intersection / union
+
         train_loss.append(np.mean(epoch_loss))
-        train_iou.append(np.mean(epoch_iou))
+        train_iou.append(np.mean(train_mean_iou))
         train_acc.append(np.mean(epoch_acc))
         val_loss.append(current_loss)
         val_iou.append(current_miou_score)
         val_acc.append(current_acc)
         scheduler.step()
 
-        if current_miou_score > best_iou_score:
-            best_iou_score = current_miou_score
+        if current_loss < best_loss:
+            best_loss = current_loss
             best_model = deepcopy(fcn_model)
             best_epoch = epoch
             counter = 0
             # save the best model
-        elif current_miou_score < best_iou_score:
+        elif current_loss > best_loss:
             counter += 1
         if counter == patience:
             print(f'Early stop at epoch {epoch}\tBest epoch: {best_epoch}')
@@ -147,8 +152,9 @@ def val(epoch):
     fcn_model.eval()  # Put in eval mode (disables batchnorm/dropout) !
 
     losses = []
-    mean_iou_scores = []
     accuracy = []
+    intersection = np.zeros(n_class)
+    union = np.zeros(n_class)
 
     with torch.no_grad():  # we don't need to calculate the gradient in the validation/testing
 
@@ -156,46 +162,47 @@ def val(epoch):
             input = input.to(device=device)
             label = label.to(device=device)
 
-            output = fcn_model.forward(input)
+            output = fcn_model(input)
 
             losses.append(criterion(output, label).item())
             accuracy.append(util.pixel_acc(output, label))
-            mean_iou_scores.append(util.iou(output, label, n_classes=n_class))
+            batch_intersection, batch_union = util.iou(output, label)
+            intersection += batch_intersection
+            union += batch_union
 
-    '''print(f"Loss at epoch: {epoch} is {np.mean(losses)}")
-    print(f"IoU at epoch: {epoch} is {np.mean(mean_iou_scores)}")
-    print(f"Pixel acc at epoch: {epoch} is {np.mean(accuracy)}")'''
+    mean_iou_scores = intersection / union
     print('Val:\t[%d/%d][1/1]\tLoss: %.4f\tIOU: %.4f\tAcc: %.4f'
           % (epoch, epochs, np.mean(losses), np.mean(mean_iou_scores), np.mean(accuracy)))
 
     fcn_model.train()  # TURNING THE TRAIN MODE BACK ON TO ENABLE BATCHNORM/DROPOUT!!
 
-    return np.mean(mean_iou_scores)
+    return np.mean(losses), np.mean(mean_iou_scores), np.mean(accuracy)
 
 
 def modelTest():
     fcn_model.eval()  # Put in eval mode (disables batchnorm/dropout) !
 
     losses = []
-    mean_iou_scores = []
     accuracy = []
+    intersection = np.zeros(n_class)
+    union = np.zeros(n_class)
 
     with torch.no_grad():  # we don't need to calculate the gradient in the validation/testing
 
-        for iter, (input, label) in enumerate(test_loader):
+        for iter, (input, label) in enumerate(val_loader):
             input = input.to(device=device)
             label = label.to(device=device)
 
-            output = fcn_model.forward(input)
+            output = fcn_model(input)
 
-            losses.append(criterion(output, label.long()).item())
+            losses.append(criterion(output, label).item())
             accuracy.append(util.pixel_acc(output, label))
-            mean_iou_scores.append(util.iou(output, label, n_classes=n_class))
+            batch_intersection, batch_union = util.iou(output, label)
+            intersection += batch_intersection
+            union += batch_union
 
-    '''print(f"Loss is {np.mean(losses)}")
-    print(f"IoU is {np.mean(mean_iou_scores)}")
-    print(f"Pixel acc is {np.mean(accuracy)}")'''
-    print('Test:\t[1/1][1/1]\tLoss: %.4f\tIOU: %.4f\tAcc: %.4f'
+    mean_iou_scores = intersection / union
+    print('Val:\t[%d/%d][1/1]\tLoss: %.4f\tIOU: %.4f\tAcc: %.4f'
           % (np.mean(losses), np.mean(mean_iou_scores), np.mean(accuracy)))
 
     fcn_model.train()  # TURNING THE TRAIN MODE BACK ON TO ENABLE BATCHNORM/DROPOUT!!

util.py
@@ -5,39 +5,16 @@ import torch.nn as nn
 
 
 def iou(pred, target, n_classes=21):
-    ious = []
+    intersections = []
+    unions = []
     pred_class = torch.argmax(pred, dim=1)
     target[target == 255] = 0
-    present_classes_per_mask = [torch.unique(mask) for mask in target]
-    for index, class_indices in enumerate(present_classes_per_mask):
-        iou_per_class = []
-        for class_index in class_indices:
-            pred_correct = torch.eq(pred_class[index], class_index)
-            target_correct = torch.eq(target[index], class_index)
-            intersection = (pred_correct & target_correct).sum().item()
-            union = (pred_correct | target_correct).sum().item()
-            iou_per_class.append(intersection / union)
-        ious.append(np.mean(iou_per_class))
-    return np.mean(ious)
-    """
-    Old implementation:
-    
-    pred_class = torch.argmax(pred, dim=1)
-    target[target == 255] = 0
-    for index in range(pred.size(0)):
-        ious_inst = []
-        pred_class_inst = pred_class[index]
-        target_inst = target[index]
-        for i in range(n_classes):
-            pred_class_positive = (pred_class_inst == i)  # torch.from_numpy(pred_class == i).to(device=device)
-            target_positive = (target_inst == i)  # torch.from_numpy(target == i).to(device=device)
-            if torch.any(target_positive):
-                cap = torch.sum(pred_class_positive * target_positive)
-                cup = torch.sum(pred_class_positive + target_positive)
-                ious_inst.append(cap.item() / cup.item())
-        ious.append(np.mean(ious_inst))
-    return np.mean(ious)
-    """
+    for index in range(n_classes):
+        pred_index = torch.eq(pred_class, index)
+        target_index = torch.eq(target, index)
+        intersections.append((pred_index & target_index).sum().item())
+        unions.append((pred_index | target_index).sum().item())
+    return np.array(intersections), np.array(unions)
 
 
 def pixel_acc(pred, target):
@@ -105,12 +82,12 @@ class DiceLoss(nn.Module):
         assert input.size(1) == self.n_class
 
         input = torch.softmax(input, dim=1)
-        input = input.permute(1, 0, 2, 3) # C x N x H x W
+        input = input.permute(1, 0, 2, 3)  # C x N x H x W
 
-        target = nn.functional.one_hot(target, num_classes=self.n_class).permute(3, 0, 1, 2) # C x N x H x W
+        target = nn.functional.one_hot(target, num_classes=self.n_class).permute(3, 0, 1, 2)  # C x N x H x W
 
-        input_flat = input.contiguous().view(self.n_class, -1) # C x N*H*W
-        target_flat = target.contiguous().view(self.n_class, -1) # C x N*H*W
+        input_flat = input.contiguous().view(self.n_class, -1)  # C x N*H*W
+        target_flat = target.contiguous().view(self.n_class, -1)  # C x N*H*W
 
         numerator = (input_flat * target_flat).sum(dim=1)
         denominator = input_flat.sum(dim=1) + target_flat.sum(dim=1)
@@ -134,8 +111,6 @@ class DiceLoss(nn.Module):
             return loss
         else:
             raise NotImplementedError
-
-
 
 
 class GeneralizedDiceLoss(nn.Module):
@@ -171,7 +146,6 @@ class GeneralizedDiceLoss(nn.Module):
         return loss
 
 
-
 class FocalLoss(nn.Module):
     def __init__(self, weight=None, gamma=2, reduction='mean'):
         super(FocalLoss, self).__init__()
@@ -196,3 +170,93 @@ class FocalLoss(nn.Module):
             return loss.sum()
         else:
             return loss
+
+
+class FocalDiceLoss(nn.Module):
+    def __init__(self, n_class, weight=None, smooth=1., gamma=1, reduction='mean'):
+        super(FocalDiceLoss, self).__init__()
+        self.n_class = n_class
+        self.weight = weight
+        self.smooth = smooth
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        assert input.size(1) == self.n_class
+
+        input = torch.softmax(input, dim=1)
+        input = input.permute(1, 0, 2, 3)  # C x N x H x W
+
+        target = nn.functional.one_hot(target, num_classes=self.n_class).permute(3, 0, 1, 2)  # C x N x H x W
+
+        input_flat = input.contiguous().view(self.n_class, -1)  # C x N*H*W
+        target_flat = target.contiguous().view(self.n_class, -1)  # C x N*H*W
+
+        numerator = ((1 - input_flat).pow(self.gamma) * input_flat * target_flat).sum(dim=1)
+        denominator = ((1 - input_flat).pow(self.gamma) * input_flat).sum(dim=1) + target_flat.sum(dim=1)
+
+        dice_scores = (2. * numerator + self.smooth) / (denominator + self.smooth)
+
+        if self.weight is not None:
+            assert self.weight.size(0) == self.n_class
+            assert len(self.weight.shape) == 1
+            class_weights = self.weight.to(device=input.device).type(input.dtype)
+            class_weights.div_(class_weights.sum())
+            dice_scores = class_weights * dice_scores
+
+        loss = 1. - dice_scores
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        elif self.reduction == 'none':
+            return loss
+        else:
+            raise NotImplementedError
+
+
+class FocalLoss2(nn.Module):
+    def __init__(self, alpha=None, gamma=0., reduction='mean', ignore_index=-100):
+
+        super(FocalLoss2, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.ignore_index = ignore_index
+        self.reduction = reduction
+
+        self.nll_loss = nn.NLLLoss(
+            weight=alpha, reduction='none', ignore_index=ignore_index)
+
+    def forward(self, input, target):
+        if input.ndim > 2:
+            # (N, C, d1, d2, ..., dK) --> (N * d1 * ... * dK, C)
+            c = input.shape[1]
+            input = input.permute(0, *range(2, input.ndim), 1).reshape(-1, c)
+            # (N, d1, d2, ..., dK) --> (N * d1 * ... * dK,)
+            target = target.view(-1)
+
+        # compute weighted cross entropy term: -alpha * log(pt)
+        # (alpha is already part of self.nll_loss)
+        log_p = nn.functional.log_softmax(input, dim=-1)
+        cross_entropy = self.nll_loss(log_p, target)
+
+        # get true class column from each row
+        all_rows = torch.arange(len(input))
+        log_pt = log_p[all_rows, target]
+
+        # compute focal term: (1 - pt)^gamma
+        pt = log_pt.exp()
+        focal_term = (1 - pt).pow( self.gamma)
+
+        # the full loss: -alpha * ((1 - pt)^gamma) * log(pt)
+        loss = focal_term * cross_entropy
+
+        if self.reduction == 'mean':
+            loss = loss.mean()
+            if self.alpha is not None:
+                loss = loss / self.alpha.sum()
+        elif self.reduction == 'sum':
+            loss = loss.sum()
+
+        return loss